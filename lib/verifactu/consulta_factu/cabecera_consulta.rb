f262533105e5d42f6ff6382693086d4c8e90d1d8
Marc Chacon <marcchaconb@gmail.com>
2025-08-20 15:00:09 +0200

    Added cabecera consulta

---
+++ lib/verifactu/consulta_factu/cabecera_consulta.rb
@@ -0,0 +1,39 @@
+module Verifactu
+  module ConsultaFactu
+    class CabeceraConsulta
+      attr_reader :id_version, :obligado_emision, :destinatario, :indicador_representante
+
+      private_class_method :new
+
+      def cabecera_obligado_emisor(obligado_emision, indicador_representante = nil)
+        raise Verifactu::VerifactuError, "obligado_emision is required" if obligado_emision.nil?
+        raise Verifactu::VerifactuError, "obligado_emision must be PersonaFisicaJuridica" unless obligado_emision.is_a(Verifactu::RegistroFacturacion::PersonaFisicaJuridica)
+        raise Verifactu::VerifactuError, "obligado_emision must have nif" if obligado_emision.nif.nil?
+        @cabecera = new(indicador_representante)
+        @fecha_expedicion_factura.instance_variable_set(:@obligado_emision, obligado_emision)
+      end
+
+      def cabecera_destinatario(destinatario, indicador_representante = nil)
+        raise Verifactu::VerifactuError, "destinatario is required" if destinatario.nil?
+        raise Verifactu::VerifactuError, "destinatario must be PersonaFisicaJuridica" unless destinatario.is_a(Verifactu::RegistroFacturacion::PersonaFisicaJuridica)
+        raise Verifactu::VerifactuError, "destinatario must have nif" if destinatario.nif.nil?
+        @cabecera = new(indicador_representante)
+        @fecha_expedicion_factura.instance_variable_set(:@destinatario, destinatario)
+      end
+
+      private
+
+      def initialize(indicador_representante = nil)
+        if indicador_representante
+          raise Verifactu::VerifactuError "indicador_representante debe estar entre #{Verifactu::Config.L1C.join(', ')} o nil" if Verifactu::Config.L1C.include(indicador_representante)
+        end
+        @id_version = Verifactu::Config.ID_VERSION
+        @indicador_representante = indicador_representante
+      end
+
+
+    end
+
+  end
+
+end
--- lib/verifactu/consulta_factu/consulta_factu.rb
+++ lib/verifactu/consulta_factu/consulta_factu.rb
@@ -8,7 +8,7 @@ module Verifactu
         raise Verifactu::VerifactuError, "Filtro de consulta is required" if filtro_consulta.nil?
         raise Verifactu::VerifactuError, "Datos adicionales de respuesta is required" if datos_adicionales_respuesta.nil?
 
-        raise Verifactu::VerifactuError, "Cabecera must be an instance of Verifactu::Cabecera" unless cabecera.is_a?(Verifactu::Cabecera)
+        raise Verifactu::VerifactuError, "Cabecera must be an instance of Verifactu::ConsultaFactu::CabeceraConsulta" unless cabecera.is_a?(Verifactu::ConsultaFactu::CabeceraConsulta)
         raise Verifactu::VerifactuError, "Filtro de consulta must be an instance of Verifactu::ConsultaFactu::FiltroConsulta" unless filtro_consulta.is_a?(Verifactu::ConsultaFactu::FiltroConsulta)
         raise Verifactu::VerifactuError, "Datos adicionales de respuesta must be an instance of Verifactu::ConsultaFactu::DatosAdicionalesRespuesta" unless datos_adicionales_respuesta.is_a?(Verifactu::ConsultaFactu::DatosAdicionalesRespuesta)
         @cabecera = cabecera
